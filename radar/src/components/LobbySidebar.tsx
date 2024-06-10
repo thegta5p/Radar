93e008207bd219116823725bbde090967bf2c488
Julian Dela Rosa-Billings <jdela055@ucr.edu>
2024-06-10 02:47:43 -0700

    cleaned up LobbySidebar comments, need to implement userInfo

--- radar/src/components/LobbySidebar.tsx
+++ radar/src/components/LobbySidebar.tsx
@@ -49,6 +49,10 @@ export default function LobbySidebar({name, game, id}) {
   // need a pointers to lobby owner, and current user
   // need to set this bool on clientside based on whether the current user is the lobby owner or not
   
+  // store uids of users in lobby
+  const [serverMemberList, setServerMemberList] = useState([]);
+
+
   useEffect(() => {
     // check for lobby owner
     async function getLobbyInfo() {
@@ -70,6 +74,7 @@ export default function LobbySidebar({name, game, id}) {
     }
     getLobbyInfo();
 }, []);
+
 
   function handleLobbyClose() {
     alert("closing lobby!");
@@ -122,74 +127,4 @@ export default function LobbySidebar({name, game, id}) {
 
 
   )
-
-
-  // return (
-  //       <Card
-  //         className="flex flex-col justify-start items-center w-64 h-full"
-  //         shadow="none"
-  //         radius="none"
-  //       >
-  //         {/* cards will be mutable by lobby owner*/}
-  //         <CardHeader className="flex flex-col">
-  //           <Tooltip content="Modfiy lobby title" isDisabled={toggleDisable}>
-  //             <Button
-  //               className="flex"
-  //               variant="light"
-  //               isDisabled={toggleDisable}
-  //             >
-  //               <p className="text-2xl font-bold text-purple-500">
-  //                 {lobbyTitle}
-  //               </p>
-  //             </Button>
-  //           </Tooltip>
-
-  //           <Spacer />
-  //           <Divider />
-  //           <Spacer />
-
-  //           <Tooltip content="Modify activity title" isDisabled={toggleDisable}>
-  //             <Button
-  //               className="flex"
-  //               variant="light"
-  //             >
-  //               <p className="text-lg font-bold">{activityTitle}</p>
-  //             </Button>
-  //           </Tooltip>
-  //         </CardHeader>
-
-  //         <CardFooter className="flex justify-center items-center">
-  //           <Card shadow="sm">
-  //             <CardHeader>
-  //               Members: {memberList.length} / {memberCap}
-  //             </CardHeader>
-
-  //             {/* maybe make user components hoverable buttons so they can be kicked by lobby owner? */}
-  //             <CardBody className="grid gap-1 grid-cols-2">
-  //               {/* {memberList.map((m) => <UserButton chatName = {m.chatName}/>)} */}
-  //               {memberList.map((m) => (
-  //                 <Dropdown key={m}>
-  //                   <DropdownTrigger>
-  //                     <Button>
-  //                       <p className="font-bold text-purple-500"> {m} </p>
-  //                     </Button>
-  //                   </DropdownTrigger>
-  //                   <DropdownMenu>
-  //                     <DropdownItem
-  //                       // className="text-danger"
-  //                       // color="secondary"
-  //                     >
-  //                       <p className="font-bold"> User Info </p>
-  //                     </DropdownItem>
-  //                   </DropdownMenu>
-  //                 </Dropdown>
-  //               ))}
-  //             </CardBody>
-  //             <CardFooter>
-  //               LobbyID = {lobbyID}
-  //             </CardFooter>
-  //           </Card>
-  //         </CardFooter>
-  //       </Card>
-  // );
 }
